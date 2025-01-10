7bc020296fae12acb07c22e91385d6d478346049
Aaron Chavez <aaron@nofoxtugiv.com>
2025-01-10 17:50:32 -0500

    +pause menu

--- scripts/global.gd
+++ scripts/global.gd
@@ -4,6 +4,11 @@ var playerScore
 var aiScore
 var maxScore = 10
 
+
 func _ready() -> void:
 	playerScore = 0
 	aiScore = 0
+
+func _process(_delta: float) -> void:
+	if aiScore == maxScore or playerScore == maxScore:
+		get_tree().reload_current_scene()
